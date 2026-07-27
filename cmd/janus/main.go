64aa284b402122a6934dc76225726cc0262cef99
p4sik <p4sikk@gmail.com>
2026-07-27 23:25:08 +0200

    show validation error

--- cmd/janus/main.go
+++ cmd/janus/main.go
@@ -11,23 +11,30 @@ import (
 )
 
 func main() {
-	workflowPath := flag.String("f", "", "path to workflow YAML file")
-	flag.Parse()
-	wf, err := workflow.ParseFile(*workflowPath)
-	if err != nil {
-		fmt.Print("Parse file error\n")
-		os.Exit(1)
-	}
-	if err := workflow.Validate(wf); err != nil {
-		fmt.Print("Validate error\n")
-		os.Exit(1)
-	}
-	ctx := context.Background()
-	e := engine.New()
-
-	fmt.Println("Janus Workflow Eninge 0.1.0")
-	if err := e.Run(ctx, wf); err != nil {
+	if err := run(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func run() error {
+	workflowPath := flag.String("f", "", "path to workflow YAML file")
+	flag.Parse()
+
+	wf, err := workflow.ParseFile(*workflowPath)
+	if err != nil {
+		return err
+	}
+
+	if err := workflow.Validate(wf); err != nil {
+		return fmt.Errorf("validate workflow: %w", err)
+	}
+
+	e := engine.New()
+
+	if err := e.Run(context.Background(), wf); err != nil {
+		return fmt.Errorf("run workflow: %w", err)
+	}
+
+	return nil
 }
